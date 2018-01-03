f6b1fe01937c01568031dd71eb3701acf5ad173a
328965413@qq.com <328965413@qq.com>
2018-01-03 17:27:45 +0800

    Test some formats.

--- baseDNNModel.py
+++ baseDNNModel.py
@@ -30,8 +30,9 @@ class BaseDNNModel(CommonModelFunc):
         name4VariableScope = "outputLayer"
         with tf.variable_scope(name4VariableScope):
           name4Weight, name4Bias, name4Act = "wOutput", "bOutput", "hOutput"
-          wHidden = self.init_weight_variable(name4Weight, 
-                                              [self.numOfNeurons[ind - 1], ele])
+          wHidden = self.init_weight_variable(
+              name4Weight,
+              [self.numOfNeurons[ind - 1], ele])
           self.variable_summaries(wHidden)
           bHidden = self.init_bias_variable(name4Bias, [ele])
           self.variable_summaries(bHidden)
@@ -46,17 +47,17 @@ class BaseDNNModel(CommonModelFunc):
           name4Weight, name4Bias = "wHidden" + str(ind), "bHidden" + str(ind)
           name4PreAct, name4Act = "preAct" + str(ind), "hHidden" + str(ind)
           wHidden = self.init_weight_variable(name4Weight,
-                                              [self.numOfNeurons[ind - 1], ele])
+              [self.numOfNeurons[ind - 1], ele])
           self.variable_summaries(wHidden)
           bHidden = self.init_bias_variable(name4Bias, [ele])
           self.variable_summaries(bHidden)
           if ind == 1:
             preAct = tf.add(tf.matmul(self.xData, wHidden),
-                            bHidden, name = name4PreAct)
+                bHidden, name = name4PreAct)
             hHidden = tf.nn.relu(preAct, name = name4Act)
           else:
             preAct = tf.add(tf.matmul(hHidden, wHidden),
-                            bHidden, name = name4PreAct)
+                bHidden, name = name4PreAct)
             hHidden = tf.nn.relu(preAct, name = name4Act)
           self.variable_summaries(preAct)
           if ind == self.numOfLayers - 2:
@@ -64,17 +65,32 @@ class BaseDNNModel(CommonModelFunc):
           self.variable_summaries(hHidden)
 
     with tf.name_scope("lossAndAccuracy"):
-      pItem = tf.reshape(tf.reduce_sum(self.yOutput, reduction_indices = [0]), [-1, 2])
-      pMistakeItem = tf.reshape(tf.reduce_sum(self.yLabel * self.yOutput, reduction_indices = [0]), [-1, 2])
+      pItem = tf.reshape(
+          tf.reduce_sum(
+              self.yOutput,
+              reduction_indices = [0]),
+          [-1, 2])
+      pMistakeItem = tf.reshape(
+          tf.reduce_sum(
+              self.yLabel * self.yOutput,
+              reduction_indices = [0]),
+          [-1, 2])
       nItem = tf.log(pItem)
       pRes = tf.matmul(pItem, tf.constant([[0.], [1.]]))
       nRes = tf.matmul(nItem, tf.constant([[1.], [0.]]))
       nMistakeRes = tf.matmul(pMistakeItem, tf.constant([[1.], [0.]]))
-      self.loss = tf.subtract(tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = self.hOutput,
-                                                                                     labels = self.yLabel)),
-                              self.FLAGS.nWeight * nMistakeRes, name = "loss")
+      self.loss = tf.subtract(
+          tf.reduce_mean(
+              tf.nn.softmax_cross_entropy_with_logits(
+                  logits = self.hOutput,
+                  labels = self.yLabel)),
+          self.FLAGS.nWeight * nMistakeRes,
+          name = "loss")
       tf.summary.scalar("lossValue", tf.reduce_mean(self.loss))
-      self.trainStep = tf.train.AdamOptimizer(self.FLAGS.learningRate).minimize(self.loss)
+      self.trainStep = tf.train.AdamOptimizer(
+          self.FLAGS.learningRate
+          ).minimize(
+              self.loss)
       correctPrediction = tf.equal(tf.argmax(self.yOutput, 1), tf.argmax(self.yLabel, 1))
       self.accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32), name = "accuracy")
       tf.summary.scalar("accuracy", self.accuracy)
